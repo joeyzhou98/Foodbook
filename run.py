c3e09b1f9748b5e3561db3a2619885fa80ad961f
joeyzhou98 <joeyzhou7002@gmail.com>
2020-01-25 12:00:51 -0500

    add html templates

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("home.html")
 
 app.run(port=5000)
