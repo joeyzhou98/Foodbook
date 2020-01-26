4e34efead5b6a8026d80ec6d98ac9370ccf950a1
joeyzhou98 <joeyzhou7002@gmail.com>
2020-01-25 23:51:56 -0500

    return uploaded picture with confidence level and name of dish

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from google.cloud.vision import types
 from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
 import requests
-app = Flask(__name__)
+app = Flask(__name__, static_folder="tempDir")
 
 # Setup Google Cloud Vision API and blacklisted labels
 client = vision.ImageAnnotatorClient()
@@ -26,7 +26,7 @@ def upload_file():
     if not os.path.exists(dirName):
         os.mkdir(dirName)
     f.save(os.path.join(dirName, file_name))
-    dishes = identify_dish(file_name)
+    dishes, img_file_name = identify_dish(file_name)
     recipies = getRecipe(dishes[0][0])
     recipiesVideos = getRecipeVideos(dishes[0][0])
     recipies_list = []
@@ -47,8 +47,8 @@ def upload_file():
         recipe["imageUrls"],
         getRecipeIngredients(recipe["id"])
       ])
-    return render_template("results.html", recipies=recipies_list, recipiesVideos=recipiesVideos_list)
-    # return recipe+"</br>Dish name: {}\nConfidence: {}".format(dishes[0][0], dishes[0][1])
+    return render_template("results.html", dish=[dishes[0][0], int(dishes[0][1] * 100), img_file_name], recipies=recipies_list, recipiesVideos=recipiesVideos_list)
+
 
 def identify_dish(img_file_name):
     file_name = os.path.abspath("tempDir/" + img_file_name)
@@ -61,7 +61,8 @@ def identify_dish(img_file_name):
     for label in labels:
         if label.description not in black_list:
             results.append((label.description, label.score))
-    return results
+    return results, img_file_name
+
 
 def getRecipe(image_dish_name):
   url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search"
@@ -73,6 +74,7 @@ def getRecipe(image_dish_name):
   response = requests.request("GET", url, headers=headers, params=querystring)
   return response.json()
 
+
 def getRecipeVideos(image_dish_name):
   url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/videos/search"
   querystring = {"query":image_dish_name}
@@ -82,6 +84,7 @@ def getRecipeVideos(image_dish_name):
       }
   response = requests.request("GET", url, headers=headers, params=querystring)
   return response.json()
+
 
 def getRecipeIngredients(recipeId):
   url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+str(recipeId)+"/ingredientWidget"
